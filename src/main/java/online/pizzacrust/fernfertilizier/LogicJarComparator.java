e666988a67af534f8c12afac995841d6f4d30a54
PizzaCrust <pizzacrustcoder@gmail.com>
2016-12-18 13:17:35 +0700

    Release golden candidate

--- src/main/java/online/pizzacrust/fernfertilizier/LogicJarComparator.java
+++ src/main/java/online/pizzacrust/fernfertilizier/LogicJarComparator.java
@@ -1,7 +1,13 @@
 package online.pizzacrust.fernfertilizier;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LogicJarComparator {
@@ -34,6 +40,28 @@ public class LogicJarComparator {
         void onRequestedWrite(String newJvmName, String oldJvmName);
 
         void onRequestedClose() throws IOException;
+
+        class SRGWriter implements MappingWriter {
+
+            private final List<String> strings = new ArrayList<String>();
+            private final File targetFile;
+
+            public SRGWriter(File targetFile) {
+                this.targetFile = targetFile;
+            }
+
+            @Override
+            public void onRequestedWrite(String newJvmName, String oldJvmName) {
+                strings.add("CL: ".concat(newJvmName).concat(" ").concat(oldJvmName));
+            }
+
+            @Override
+            public void onRequestedClose() throws IOException {
+                Files.write(targetFile.toPath(), strings, Charset.defaultCharset(),
+                        StandardOpenOption.TRUNCATE_EXISTING);
+            }
+
+        }
 
     }
 
---
+++ src/main/java/online/pizzacrust/fernfertilizier/Main.java
@@ -0,0 +1,41 @@
+package online.pizzacrust.fernfertilizier;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class Main {
+
+    private static List<String> toList(String... strings) {
+        return Arrays.asList(strings);
+    }
+
+    public static void main(String[] args) throws Exception {
+        OptionParser parser = new OptionParser();
+        parser.acceptsAll(toList("original", "1", "o", "f"), "Sets the ORIGINAL (deobf) JAR.")
+                .withRequiredArg().ofType(File.class).required();
+        parser.acceptsAll(toList("new", "2", "n", "s"), "Sets the SECOND (obf) or updated JAR" +
+                ".").withRequiredArg().ofType(File.class).required();
+        parser.acceptsAll(toList("help", "?", "h"), "Launches the HELP menu.").forHelp();
+        parser.acceptsAll(toList("passgrade", "percentage", "p", "pg", "g", "grade"), "Sets the" +
+                " passing grade.").withRequiredArg().ofType(Integer.class).required();
+        OptionSet arguments = parser.parse(args);
+        File originalJar = (File) arguments.valueOf("original");
+        File newJar = (File) arguments.valueOf("new");
+        if (arguments.has("passgrade")) {
+            ClassComparator.PASSING_PERCENTAGE = (int) arguments.valueOf("passgrade");
+            System.out.println("Passing percentage grade is set to: " + ClassComparator.PASSING_PERCENTAGE);
+        }
+        LogicalJar logicOriginal = LogicalJar.process(originalJar);
+        LogicalJar logicalNew      = LogicalJar.process(newJar);
+        LogicJarComparator comparator = new LogicJarComparator(logicOriginal, logicalNew);
+        File srgFile = new File(System.getProperty("user.dir"), "target.srg");
+        System.out.println("NOTICE: Writing output SRG to: " + srgFile.getAbsolutePath());
+        comparator.writeTo(new LogicJarComparator.MappingWriter.SRGWriter(srgFile));
+        System.out.println("SRG has been written with mappings!");
+    }
+
+}
